bf0742fec7423f3f700864e7bc125efbc8be7cb3
bit mechanix llc <enrique@bit-mechanix.com>
2024-11-14 01:05:00 -0500

    not needed

--- models/catalog.js
+++ models/catalog.js
@@ -6,7 +6,7 @@ const field = (json, idx, idx2) => fields(json, idx)[idx2].elements[0].text
 const id = (json, idx) => course(json, idx).elements[0].elements[0].text
 const title = (json, idx) => course(json, idx).elements[1].elements[0].text
 const description = (json, idx) => course(json, idx).elements[1].elements[0].text
-const parser = { catalog, courses, course, fields, field, id, title, description }; console.log(parser.courses)
+const parser = { catalog, courses, course, fields, field, id, title, description }
 
 
 export default parser
